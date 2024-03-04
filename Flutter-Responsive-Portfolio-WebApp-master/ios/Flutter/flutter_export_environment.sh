1f57653911afcff6faa994c54f3e1bfa3ddda9ff
shivendramani <shivendramanitripathi549@gmail.com>
2024-03-05 02:54:38 +0530

    fix

--- Flutter-Responsive-Portfolio-WebApp-master/ios/Flutter/flutter_export_environment.sh
+++ Flutter-Responsive-Portfolio-WebApp-master/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\developer\flutternew\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Flutter-Responsive-Portfolio-WebApp-master"
+export "FLUTTER_APPLICATION_PATH=D:\flutter-portfolio\myprofile\Flutter-Responsive-Portfolio-WebApp-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
