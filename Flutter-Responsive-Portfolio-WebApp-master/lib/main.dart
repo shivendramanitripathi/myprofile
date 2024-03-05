d11094940e092ff22d0e6ae70851e5b848c57574
shivendramani <shivendramanitripathi549@gmail.com>
2024-03-05 12:57:54 +0530

    fix

--- Flutter-Responsive-Portfolio-WebApp-master/lib/main.dart
+++ Flutter-Responsive-Portfolio-WebApp-master/lib/main.dart
@@ -6,27 +6,26 @@ import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        scaffoldBackgroundColor: bgColor,
-        useMaterial3: true,
-        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white,)
-            .copyWith(
-          bodyText1: const TextStyle(color: bodyTextColor),
-          bodyText2: const TextStyle(color: bodyTextColor),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          scaffoldBackgroundColor: bgColor,
+          useMaterial3: true,
+          textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
+              .apply(
+                bodyColor: Colors.white,
+              )
+              .copyWith(
+                bodyText1: const TextStyle(color: bodyTextColor),
+                bodyText2: const TextStyle(color: bodyTextColor),
+              ),
         ),
-      ),
-
-      home: const SplashView()
-    );
+        home: const SplashView());
   }
 }
-
-
